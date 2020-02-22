fd9bcb90a2a8aa44000db0e98441fd66dc52d4f7
root <watson@example.com>
2020-02-22 16:34:54 +0800

    fix add jpeg image copied from blog. 	modified:   pull.c

--- pull/pull.c
+++ pull/pull.c
@@ -13,12 +13,112 @@ AVStream *stream;
 AVCodecContext *pCodecCtx;
 AVCodec *pCodec;
 AVFrame *per_frame;
+
+#if PNM
 AVFrame *rgb_frame;
+#endif
 
 #define URL "rtmp://120.77.214.213:1935/live_video/video"
 #define OUT "helloworld.pnm"
 #define JPG "hello.jpg"
 #define VERSION 1.01
+#define PNM 0
+#define JPEG 1
+
+int save_jpeg(AVFrame *pFrame, char *out_name)
+{
+    AVCodecContext *pCodeCtx = NULL;
+    
+    AVFormatContext *pFormatCtx = avformat_alloc_context();
+
+    //output format as mjpeg
+    pFormatCtx->oformat = av_guess_format("mjpeg", NULL, NULL);
+
+    if (avio_open(&pFormatCtx->pb, out_name, AVIO_FLAG_READ_WRITE) < 0) {
+        printf("Couldn't open output file.");
+        return -1;
+    }
+
+    AVStream *pAVStream = avformat_new_stream(pFormatCtx, 0);
+    if (pAVStream == NULL) {
+        return -1;
+    }
+
+    AVCodecParameters *parameters = pAVStream->codecpar;
+    parameters->codec_id = pFormatCtx->oformat->video_codec;
+    parameters->codec_type = AVMEDIA_TYPE_VIDEO;
+    parameters->format = AV_PIX_FMT_YUVJ420P;
+    parameters->width = pFrame->width;
+    parameters->height = pFrame->height;
+
+    AVCodec *pCodec = avcodec_find_encoder(pAVStream->codecpar->codec_id);
+
+    if (!pCodec) {
+        printf("Could not find encoder\n");
+        return -1;
+    }
+
+    pCodeCtx = avcodec_alloc_context3(pCodec);
+    if (!pCodeCtx) {
+        fprintf(stderr, "Could not allocate video codec context\n");
+        exit(1);
+    }
+
+    if ((avcodec_parameters_to_context(pCodeCtx, pAVStream->codecpar)) < 0) {
+        fprintf(stderr, "Failed to copy %s codec parameters to decoder context\n",
+                av_get_media_type_string(AVMEDIA_TYPE_VIDEO));
+        return -1;
+    }
+
+    pCodeCtx->time_base = (AVRational) {1, 25};
+
+	if (avcodec_open2(pCodeCtx, pCodec, NULL) < 0) {
+        printf("Could not open codec.");
+        return -1;
+    }
+
+    int ret = avformat_write_header(pFormatCtx, NULL);
+    if (ret < 0) {
+        printf("write_header fail\n");
+        return -1;
+    }
+
+    //Encode
+    AVPacket pkt;
+    av_new_packet(&pkt, (pFrame->width)*(pFrame->height) * 3);
+
+	/*send frame*/
+    ret = avcodec_send_frame(pCodeCtx, pFrame);
+    if (ret < 0) {
+        printf("Could not avcodec_send_frame.");
+        return -1;
+    }
+	/*receive decode packet*/
+    ret = avcodec_receive_packet(pCodeCtx, &pkt);
+    if (ret < 0) {
+        printf("Could not avcodec_receive_packet");
+        return -1;
+    }
+	
+	/*write the data to file*/
+    ret = av_write_frame(pFormatCtx, &pkt);
+
+    if (ret < 0) {
+        printf("Could not av_write_frame");
+        return -1;
+    }
+
+    av_packet_unref(&pkt);
+
+	/*write the end of file*/
+    av_write_trailer(pFormatCtx);
+
+    avcodec_close(pCodeCtx);
+    avio_close(pFormatCtx->pb);
+    avformat_free_context(pFormatCtx);
+	return 0;
+}
+
 
 void img_convert_jpeg(int width, int height)
 {
@@ -71,12 +171,15 @@ void test_ffmpeg_rtmp_client()
 	int ret = -1;
 	int video_stream_index = -1;
 	int audio_stream_index = -1;
+#if PNM
 	int numbytes;
 	int got_picture;
 	int size;
-	int index = 0;
 	uint8_t *out_buffer;
 	struct SwsContext *img_convert_ctx;
+#endif
+	int index = 0;
+	char filename[32] = "";
 
 	//init register and network
 	init_register_network();
@@ -140,12 +243,14 @@ void test_ffmpeg_rtmp_client()
 
 	//alloc for rgb memory
 	per_frame = av_frame_alloc();
+#if PNM
 	rgb_frame = av_frame_alloc();
 	img_convert_ctx = sws_getContext(pCodecCtx->width,pCodecCtx->height,pCodecCtx->pix_fmt,pCodecCtx->width,pCodecCtx->height,AV_PIX_FMT_RGB24, SWS_BICUBIC, NULL, NULL, NULL);
 	numbytes = avpicture_get_size(AV_PIX_FMT_RGB24, pCodecCtx->width,pCodecCtx->height);
 	out_buffer = (uint8_t *) av_malloc(numbytes * sizeof(uint8_t));
 	avpicture_fill((AVPicture *) rgb_frame, out_buffer, AV_PIX_FMT_RGB24,pCodecCtx->width, pCodecCtx->height);
 	size = pCodecCtx->width * pCodecCtx->height;
+#endif
 
 	/*
 	*	read each frame using av_read_frame
@@ -155,7 +260,9 @@ void test_ffmpeg_rtmp_client()
 
 	/*avoid segmentation fault*/
 	pkt = av_packet_alloc();
+#if PNM
 	av_new_packet(pkt,size);
+#endif
 	av_dump_format(in_format_ctx,0,OUT,0);
 
 	while (1) 
@@ -167,12 +274,14 @@ void test_ffmpeg_rtmp_client()
 			fprintf(stderr, "error or end of file: %d\n", ret);
 			continue;
 		}
+		fprintf(stdout, "video stream, packet size: %d\n", pkt->size);
 		//index of av 
 		if (pkt->stream_index == video_stream_index)
 		{
+#if PNM
 			/*decode video*/
 			ret = avcodec_decode_video2(pCodecCtx,per_frame,&got_picture,pkt);
-
+#endif
 			/*pkt->size should not be zero, if it is, it should be break*/
 			if (ret < 0 && !(pkt->size))
 			{
@@ -180,9 +289,10 @@ void test_ffmpeg_rtmp_client()
 				return;
 			}
 			/*If one picture comes it should be storage.*/
-			if (got_picture)
+			if (JPEG)
 			{
 				index++;
+#if PNM
 				sws_scale(img_convert_ctx,(uint8_t const *) per_frame->data,per_frame->linesize, 0, pCodecCtx->height, rgb_frame->data,rgb_frame->linesize);
 				if (index == 1)
 				{
@@ -191,6 +301,16 @@ void test_ffmpeg_rtmp_client()
 					//img_convert_jpeg(pCodecCtx-> width, pCodecCtx->height);
 					if (ret)
 						return;
+				}
+#endif
+				/*avcodec send packet*/
+				if (avcodec_send_packet(pCodecCtx,pkt) < 0)
+					continue;
+				/*receive packet*/
+				while(avcodec_receive_frame(pCodecCtx,per_frame) == 0)
+				{
+					sprintf(filename,"jpg/NO_%d.jpg",index);	
+					save_jpeg(per_frame,filename);//save image as jpeg	
 				}
 			}
 
@@ -204,8 +324,10 @@ void test_ffmpeg_rtmp_client()
 
 	//free all 
 	avformat_free_context(in_format_ctx);
+#if PNM
 	av_free(rgb_frame);
 	av_free(out_buffer);
+#endif
 	avcodec_close(pCodecCtx);
 	avformat_close_input(&in_format_ctx);
 }
